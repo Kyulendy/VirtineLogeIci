b47282ff527838fb1bddbb8b3e5c56dab62c3d23
Ekaterina Johnston <ekaterinaj@theodo.fr>
2014-03-14 18:09:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 VirtineLogeIci
 ==============
 
-Site Vitrine pour le projet PFE Sup'Internet Loge'Ici
+Site Vitrine pour le projet PFE Sup'Internet promo 2014 "Loge'Ici"
+
+Equipe:
+    David Tang - Chef de Projet
+    Jonas Loco - Responsable Com'
+    Brian Marciano - Responsable Marketing
+    Samuel Bodin - Lead Developer
+    Ekaterina Johnston - Développeur
+    Josué Studer - Directeur Artistique
+    Sébastien Tsing - Web Designer
---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Loge ici</title>
+    <link rel="stylesheet" href="reset.css" type="text/css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+</head>
+<body>
+<div id="top-section" class="section">
+    <div class="content">
+        <div id="drop" class="center"></div>
+        <div id="logo" class="center">
+            <h1>Loge ici</h1>
+            <p id="subtitle">et pas ailleurs</p>
+        </div>
+    </div>
+</div>
+<div id="buildings">
+    <div id="chevron" class="center"></div>
+</div>
+
+<div id="tagline-section" class="section">
+    <div class="content">
+        <p class="tagline">
+            Combien de fois avons-nous eu l’occasion de visiter des dizaines de sites de
+            <br/>recherche immobilière, regorgés de divers fonctionnalités
+            <br/> mais ne répondant pas toujours à nos besoins ?
+        </p>
+        <p class="tagline bold">
+            Nous avons donc décidé de vous proposer une solution afin
+            <br/>et de vous offrir l’opportunité de trouver un logement dans
+            <br/>le quartier de votre choix de façon simple et efficace.</p>
+        <hr id="line">
+    </div>
+</div>
+<div id="points-section" class="section">
+    <div class="content">
+        <div class="point point-text-right">
+            <div>
+                <div id="first-oval" class="oval"></div>
+            </div>
+            <div>
+                <h3>Un système de recherche optimisé</h3>
+                <p>Visualisez exactement où ce situe le logement souhaité en fonction de votre lieu de travail, cercle d’amis... et prenez connaissance de la vie du quartier.</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="point point-text-left">
+            <div>
+                <h3>Des résultats de recherche plus lisibles</h3>
+                <p>Sélectionnez et comparez uniquement les annonces qui vous intéressent et masquer celles qui ne le sont pas. Terminés les résultats noyés dans la masse.</p>
+            </div>
+            <div>
+                <div id="second-oval" class="oval"></div>
+            </div>
+        </div>
+        <div class="clear"></div>
+        <div class="point point-text-right">
+            <div>
+                <div id="third-oval" class="oval"></div>
+                <div class="clear"></div>
+            </div>
+            <div>
+                <h3>Une candidature en 1 clic !</h3>
+                <p>En un clic, mettez vos chances de votre côté et laissez votre candidature au propriétaire. Celui-ci prendra directement contact avec vous si votre profil l'intéresse.</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div id="invitation-section" class="section">
+    <div class="content">
+        <div id="invitation-block">
+            <h2>Invitation</h2>
+            <p id="subtitle">Vous avez hâte de découvrir Loge'ici ?</p>
+            <p>
+                Recevez votre invitation et ayez ainsi le privilège de
+                <br/>tester en avant-première notre plateforme.
+            </p>
+            <div id="email-form">
+            <form name="subscribe" action="#invitation-block" method="GET">
+                <input type="email" placeholder="Votre email" name="email">
+                <input type="submit" value="Go">
+            </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="team-section" class="section">
+    <div class="content">
+        <img class="center" alt="Exclusive Diversity" src="./images/ed_logo.png"/>
+        <h4>We are Exclusive Diversity</h4>
+        <p>Créer en 2013 au sein d’une école du web, cette passion nous a réuni naturellement de part notre envie
+            <br/>commune de bousculer les standards présent sur le web. Ayant des compétences très diverses,
+            <br/>complémentarité et notre esprit d’équipe nous permettent de travailler sur des projets
+            <br/>d’envergure et atteindre les différents objectifs que nous nous fixerons.</p>
+        <br/>
+        <p class="bold">« La diversité unit dans un projet commun »</p>
+        <div id="members">
+            <div>
+                <img src="./images/members/david.png">
+                <p>David Tang</p>
+                <p class="light">Chef de Projet</p>
+            </div>
+            <div>
+                <img src="./images/members/jonas.png">
+                <p>Jonas Loco</p>
+                <p class="light">Responsable Com'</p>
+            </div>
+            <div>
+                <img src="./images/members/brian.png">
+                <p>Brian Marciano</p>
+                <p class="light">Responsable Marketing</p>
+            </div>
+            <div>
+                <img src="./images/members/samuel.png">
+                <p>Samuel Bodin</p>
+                <p class="light">Lead Developer</p>
+            </div>
+            <div>
+                <img src="./images/members/ekaterina.png">
+                <p>Ekaterina Johnston</p>
+                <p class="light">Développeur</p>
+            </div>
+            <div>
+                <img src="./images/members/josue.png">
+                <p>Josué Studer</p>
+                <p class="light">Directeur Artistique</p>
+            </div>
+            <div>
+                <img src="./images/members/sebastien.png">
+                <p>Sébastien Tsing</p>
+                <p class="light">Web Designer</p>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</div>
+<div class="teardrop"></div>
+</body>
+</html>
